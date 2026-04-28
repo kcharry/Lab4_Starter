ac945160eb3ea657b023ee4da8ca0124b90b4dce
kcharry <kathycharry27@gmail.com>
2026-04-27 21:06:24 -0700

    fixing functions in lab4.js

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -7,14 +7,14 @@
  */
 function sumValues(num1, num2, add) {
     if (add) {
-        const result = 0;
+        let result = 0;
 
         result = num1 + num2;
 
         return result;
     }
     else {
-        return !add;
+        return add;
     }
 }
 
@@ -26,10 +26,15 @@ function sumValues(num1, num2, add) {
  */
 function discountPrices(prices, discount) {
     const discounted = []
+    
     const length = prices.length;
+    if (length == 0){
+        return false;
+    }
+
     let discountedPrice = 0
     for(let i = 0; i < length; i++) {
-        discountedPrice += prices[i] * (1 - discount);
+        discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
 
@@ -37,3 +42,8 @@ function discountPrices(prices, discount) {
 }
 
 module.exports = {sumValues, discountPrices};
+
+console.log(sumValues(2, 3, true));
+console.log(sumValues(2, 3, false));
+console.log(discountPrices([100, 200, 300], 0.5));
+console.log(discountPrices([], 0.5));
