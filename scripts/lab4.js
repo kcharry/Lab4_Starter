5e0c9796a08fdeb4f714583db1aa3cf03acb32b5
kcharry <kathycharry27@gmail.com>
2026-04-27 21:20:24 -0700

    correct value --> add in sumValues

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -7,9 +7,10 @@
  */
 function sumValues(num1, num2, add) {
     if (!(typeof num1 === "number") 
-        || !(typeof num2 === "number") || !(typeof value === "boolean")){
+        || !(typeof num2 === "number") || !(typeof add === "boolean")){
         return false;
     }
+
     if (add) {
         let result = 0;
 
